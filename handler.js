42f148c83e3dd022dc4c4feea27fe164d73f3b96
HOTELGROUP\reilljoh <john.reilly@wyn.com>
2017-09-19 12:11:18 -0400

    first commit

---
+++ handler.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const fetch = require('node-fetch');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const s3 = new AWS.S3();
+
+module.exports.save = (event, context, callback) => {
+  fetch(event.url)
+    .then((response) => {
+      if (response.ok) {
+        return response;
+      }
+      return Promise.reject(new Error(
+            `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`));
+    })
+    .then(response => response.buffer())
+    .then(buffer => (callback(null, buffer), callback);
+};
